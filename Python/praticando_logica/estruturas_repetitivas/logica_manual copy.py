a334c5979c13c9819cd430dcd3c0cec7ee4e9bed
andrefischborn <andrefischborn@hotmail.com>
2022-12-01 18:12:54 -0300

    Adicionado novos exercicios de lógica (Estruturas Repetitivas - 'While')

--- Python/praticando_logica/estruturas_repetitivas/logica_manual copy.py
+++ Python/praticando_logica/estruturas_repetitivas/logica_manual copy.py
@@ -1,9 +1,10 @@
 '''
 Esse exercício foi realizado em uma folha de papel. Está escrito aqui apenas para registro em código real.
 '''
-x = 0
-y = 4
-while x < 3:
-    y = y + 2
-    x = x + 1
-    print(f'{x} - {y}')
+x = 100
+y = 100
+
+while x == y:    
+    x = float(y) ** 0.5     # Raiz quadrada de Y
+    print(f'{x} - {y}') 
+
---
+++ Python/praticando_logica/estruturas_repetitivas/problema_meia_idades.py
@@ -0,0 +1,37 @@
+'''
+    Problema "media_idades"
+Faça um programa para ler um número indeterminado de dados, contendo cada um, a idade de um
+indivíduo. O último dado, que não entrará nos cálculos, contém um valor de idade negativa. Calcular
+e imprimir a idade média deste grupo de indivíduos. Se for entrado um valor negativo na primeira vez,
+mostrar a mensagem "IMPOSSIVEL CALCULAR".
+    Exemplo 1:
+        Digite as idades:
+        31
+        27
+        46
+        -5
+        MEDIA = 34.67
+    Exemplo 2:
+        Digite as idades:
+        -10
+        IMPOSSIVEL CALCULAR 
+'''
+print('Digite as idades: ')
+idades = int(input())
+soma = 0
+cont = 0
+
+while idades >= 0:
+    soma = soma + idades  
+    cont = cont + 1
+    print('Digite outra idade: ')
+    idades = int(input())
+
+if cont == 0:
+    print(f'IMPOSSÍVEL CALCULAR.')
+else:
+    media = soma / cont
+    print(f'MÉDIA: %.2f' % (media))   
+
+
+
