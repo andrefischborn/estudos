1bffc5a2c53bc4977505e42bce38a0a7f84fd18e
andrefischborn <andrefischborn@hotmail.com>
2023-02-06 17:20:01 -0300

    Novo

---
+++ Python/exercicios_udemy/aula_48_map_lista.py
@@ -0,0 +1,18 @@
+'''
+MAP FUNCTION
+    Muito utilizado em listas
+    Aplicar uma função a um Iterable, por item, (Lista, tuple, dic, etc)
+
+'''
+
+lista1 = [1, 2, 3, 4 ]
+
+def multi(x):
+    return x * 2
+
+
+lista2= map(multi, lista1)
+
+print(lista1)
+print(list(lista2)) # Se não colocar o "list", a impressão será um OBJETO:
+print(lista2)
